dd9b007cf41847a0ae60fd1b619ee946f3364b87
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-02 00:05:52 +0300

    Restructuted the app -MVC model

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 
+// Routes
+const authRoutes = require("./routes/AuthRoute");
+
 const app = express();
 
+// Middlewares
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use("/auth", authRoutes);
 
 dotenv.config();
 mongoose.set("strictQuery", true);
