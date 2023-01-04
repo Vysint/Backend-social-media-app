f93d0d6b6a9814bc5018e611768d84e189f35bef
Vincent Kipngetich <vinniebluz@icloud.com>
2023-01-04 17:04:28 +0300

    Added user Update - CRUD Operation

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 const User = require("../models/user");
 
 exports.getUser = async (req, res) => {
@@ -19,14 +21,20 @@ exports.getUser = async (req, res) => {
 exports.updateUser = async (req, res) => {
   const id = req.params.id;
 
-  const { userId, userAdminStatus} = req.body;
+  const { userId, userAdminStatus, password } = req.body;
 
   if (id === userId || userAdminStatus) {
     try {
+      if (password) {
+        req.body.password = await bcrypt.hash(password, 12);
+      }
       const user = await User.findByIdAndUpdate(id, req.body, { new: true });
       res.status(200).json(user);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
+  }
+  else{
+    res.status(403).json("Acces Denied! You can only update your profile")
   }
 };
